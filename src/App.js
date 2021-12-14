0ad0b217174076a83e6e53ebfe9e350828a5a972
sean <sean.richardson.umd18@gmail.com>
2021-12-14 12:04:06 -0500

    Required data.js, displayed routes data in table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import data from './data.js';
+const { routes, airlines, airports } = data;
 
-const App = () => (
-  <div className="app">
-  <header className="header">
-    <h1 className="title">Airline Routes</h1>
-  </header>
-  <section>
-    <p>
-      Welcome to the app!
-    </p>
-  </section>
-</div>
-)
+const App = () => {
+  return (
+    <div className="app">
+      <header className="header">
+        <h1 className="title">Airline Routes</h1>
+      </header>
+      <section>
+        <table>
+          <thead>
+            <tr>
+              <th>Airline</th>
+              <th>Source Airport</th>
+              <th>Destination Airport</th>
+            </tr>
+          </thead>
+          <tbody>
+            {routes.map(({ airline, src, dest }) => {
+              return (<tr>
+                <td>{airline}</td>
+                <td>{src}</td>
+                <td>{dest}</td>
+                </tr>)
+              })}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  )
+}
 
 export default App;

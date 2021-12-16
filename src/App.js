6ff6a957f64afbc1b7e678e5ceed624c3b1c19b0
sean <sean.richardson.umd18@gmail.com>
2021-12-15 20:17:04 -0500

    Added airline filter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Table from "./components/Table";
 import data, { getAirlineById, getAirportByCode } from "./data.js";
@@ -23,16 +23,44 @@ const rows = routes.map((route) => {
 const rowsPerPage = 25;
 
 const App = () => {
+  const [filteredRows, setFilteredRows] = useState(rows);
+
+  const handleSelection = (e) => {
+    const airlineFilter = document.getElementById("airline").value;
+
+    setFilteredRows(
+      rows.filter((row) => {
+        const matchesAirline =
+          airlineFilter === "all" || row.airline === airlineFilter;
+
+        return matchesAirline;
+      })
+    );
+  };
+
   return (
     <div className="app">
       <header className="header">
         <h1 className="title">Airline Routes</h1>
       </header>
       <section>
+        <p>
+          Show routes on
+          <select id="airline" onChange={handleSelection}>
+            <option value="all">All Airlines</option>
+            {airlines.map((airline) => {
+              return (
+                <option key={airline.id} value={airline.name}>
+                  {airline.name}
+                </option>
+              );
+            })}
+          </select>
+        </p>
         <Table
           className="routes-table"
           columns={columns}
-          rows={rows}
+          rows={filteredRows}
           perPage={rowsPerPage}
           format=""
         />

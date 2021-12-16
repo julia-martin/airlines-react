7eae4a9654a4153eb2ddd8559082ea226353153b
sean <sean.richardson.umd18@gmail.com>
2021-12-15 20:16:11 -0500

    Now end index can't be bigger than total rows

--- src/components/TableMessage.js
+++ src/components/TableMessage.js
@@ -1,9 +1,10 @@
 import React from "react";
 
 const TableMessage = ({ firstN, totalRows, perPage }) => {
+  const lastN = firstN + perPage > totalRows ? totalRows : firstN + perPage;
   return (
     <p>
-      Showing {firstN + 1}-{firstN + perPage} of {totalRows} routes.
+      Showing {firstN + 1}-{lastN} of {totalRows} routes.
     </p>
   );
 };
